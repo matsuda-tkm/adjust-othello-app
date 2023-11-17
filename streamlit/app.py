e4ddb893ebe74882533f8feb213d076a6c089b2c
Matsuda <matsu20010403@gmail.com>
2023-11-18 01:21:59 +0900

    fix path

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.append('../')
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import streamlit as st
 from creversi import *
 from network import ValueNetwork, PolicyNetwork
